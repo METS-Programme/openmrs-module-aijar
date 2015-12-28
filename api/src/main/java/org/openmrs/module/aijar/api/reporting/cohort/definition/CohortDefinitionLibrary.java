102d72255f9057ecaeeeb45a856fbcc67e83282f
ssmusoke <ssmusoke@gmail.com>
2015-12-28 18:08:32 +0300

    Added JavaDoc descriptions for the CohortDefinitions

--- api/src/main/java/org/openmrs/module/aijar/api/reporting/cohort/definition/CohortDefinitionLibrary.java
+++ api/src/main/java/org/openmrs/module/aijar/api/reporting/cohort/definition/CohortDefinitionLibrary.java
@@ -1,11 +1,12 @@
 package org.openmrs.module.aijar.api.reporting.cohort.definition;
 
+import org.openmrs.Concept;
 import org.openmrs.EncounterType;
-import org.openmrs.api.context.Context;
-import org.openmrs.module.reporting.cohort.definition.AgeCohortDefinition;
-import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
-import org.openmrs.module.reporting.cohort.definition.EncounterCohortDefinition;
-import org.openmrs.module.reporting.cohort.definition.GenderCohortDefinition;
+import org.openmrs.api.PatientSetService;
+import org.openmrs.module.aijar.api.reporting.Dictionary;
+import org.openmrs.module.reporting.cohort.definition.*;
+import org.openmrs.module.reporting.common.SetComparator;
+import org.openmrs.module.reporting.common.TimeQualifier;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +18,11 @@ import java.util.Date;
  */
 @Component
 public class CohortDefinitionLibrary {
-
+    /**
+     * Patients who are male
+     *
+     * @return the cohort definition
+     */
     public CohortDefinition getMales() {
         GenderCohortDefinition males = new GenderCohortDefinition();
         males.setName("Males");
@@ -25,6 +30,10 @@ public class CohortDefinitionLibrary {
         return males;
     }
 
+    /**
+     * Patients who are female
+     * @return the cohort definition
+     */
     public CohortDefinition getFemales() {
         GenderCohortDefinition males = new GenderCohortDefinition();
         males.setName("Females");
@@ -32,6 +41,10 @@ public class CohortDefinitionLibrary {
         return males;
     }
 
+    /**
+     * Patients who at most maxAge years old on ${effectiveDate}
+     * @return the cohort definition
+     */
     public CohortDefinition getAgeAtMost(int ageInYears) {
         AgeCohortDefinition ageAtMost = new AgeCohortDefinition();
         ageAtMost.setName("Age at Most " + ageInYears + " years");
@@ -41,6 +54,10 @@ public class CohortDefinitionLibrary {
         return ageAtMost;
     }
 
+    /**
+     * Patients who are at least minAge years old on ${effectiveDate}
+     * @return the cohort definition
+     */
     public CohortDefinition getAgeAtLeast(int ageInYears) {
         AgeCohortDefinition ageAtLeast = new AgeCohortDefinition();
         ageAtLeast.setName("Age at Least " + ageInYears + " years");
@@ -50,6 +67,10 @@ public class CohortDefinitionLibrary {
         return ageAtLeast;
     }
 
+    /**
+     * Patients who are between minAge years old and maxAge years old on ${effectiveDate}
+     * @return the cohort definition
+     */
     public CohortDefinition getAgeInRange(int minAge, int maxAge) {
         AgeCohortDefinition ageInRange = new AgeCohortDefinition();
         ageInRange.setName("Age between " + minAge + " years and " + maxAge + " years");
@@ -60,20 +81,61 @@ public class CohortDefinitionLibrary {
         return ageInRange;
     }
 
-    public CohortDefinition cumulativeEnrolled() {
-        // Summary Page Encounter Type
-        // TODO: Change this to the ART Card Summary page as the EID card will also have a summary page
-        EncounterType enrolledInCare = Context.getEncounterService().getEncounterTypeByUuid("8d5b27bc-c2cc-11de-8d13-0010c6dffd0f");
-        EncounterCohortDefinition cumulativeEnrolled = new EncounterCohortDefinition();
-        cumulativeEnrolled.setName("Cumulative Enrolled");
-
-        cumulativeEnrolled.setEncounterTypeList(Arrays.asList(enrolledInCare));
-        cumulativeEnrolled.addParameter(new Parameter("onOrAfter", "Start Date", Date.class));
-        cumulativeEnrolled.addParameter(new Parameter("onOrBefore", "End Date", Date.class));
-
-        return cumulativeEnrolled;
-
+    /**
+     * Patients who have an obs between ${onOrAfter} and ${onOrBefore}
+     *
+     * @param question the question concept
+     * @param answers  the answers to include
+     * @return the cohort definition
+     */
+    public CohortDefinition hasObs(Concept question, Concept... answers) {
+        CodedObsCohortDefinition cd = new CodedObsCohortDefinition();
+        cd.setName("has obs between dates");
+        cd.setQuestion(question);
+        cd.setOperator(SetComparator.IN);
+        cd.setTimeModifier(PatientSetService.TimeModifier.ANY);
+        cd.addParameter(new Parameter("onOrBefore", "Before Date", Date.class));
+        cd.addParameter(new Parameter("onOrAfter", "After Date", Date.class));
+        if (answers.length > 0) {
+            cd.setValueList(Arrays.asList(answers));
+        }
+        return cd;
     }
+
+    /**
+     * Patients who have an encounter between ${onOrAfter} and ${onOrBefore}
+     *
+     * @param types the encounter types
+     * @return the cohort definition
+     */
+    public CohortDefinition hasEncounter(EncounterType... types) {
+        EncounterCohortDefinition cd = new EncounterCohortDefinition();
+        cd.setName("has encounter between dates");
+        cd.setTimeQualifier(TimeQualifier.ANY);
+        cd.addParameter(new Parameter("onOrBefore", "Before Date", Date.class));
+        cd.addParameter(new Parameter("onOrAfter", "After Date", Date.class));
+        if (types.length > 0) {
+            cd.setEncounterTypeList(Arrays.asList(types));
+        }
+        return cd;
+    }
+
+    /**
+     * Patients who have an upcoming appointment in between ${onOrAfter} and ${onOrBefore}
+     *
+     * @return the cohort definition
+     */
+    public CohortDefinition nextAppointment() {
+        Concept transferInDate = Dictionary.getConcept("5096");
+
+        DateObsCohortDefinition cd = new DateObsCohortDefinition();
+        cd.setName("transferred in between dates");
+        cd.setQuestion(transferInDate);
+        cd.addParameter(new Parameter("onOrAfter", "After Date", Date.class));
+        cd.addParameter(new Parameter("onOrBefore", "Before Date", Date.class));
+        return cd;
+    }
+
 
 
 }
