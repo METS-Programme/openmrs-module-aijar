e8711f6fe21d69554465cf6fc55ef83cc85ba642
ssmusoke <ssmusoke@gmail.com>
2016-02-08 18:14:52 +0300

    Added functionality to hide any empty patient identifiers

--- omod/src/main/webapp/resources/js/aijar.js
+++ omod/src/main/webapp/resources/js/aijar.js
@@ -7,3 +7,17 @@ jq('#checkbox-autogenerate-identifier').click(function () {
         NavigatorController.getFieldById('deceased-status').hide();
     }
 })
+
+/* Remove the ability to edit the different patient identifiers by removing the link */
+
+jq(".editPatientIdentifier").each(function () {
+    // check if the contents is Add then remove it along with the em before which is empty
+    if (jq(this).text() == 'Add') {
+        jq(this).parent().prev("em").hide(); // hide the label for the patient identifier which is the EM just before the
+        // parent span containing the link
+        jq(this).hide(); // hide this link
+    } else {
+        // remove the link to editing the patient identifier
+        jq(this).removeAttr("href");
+    }
+});
