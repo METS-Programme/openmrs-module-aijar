1619bc2cc2401a37b0a4ca43827057d983f31aac
ssmusoke <ssmusoke@gmail.com>
2016-03-04 18:28:24 +0300

    Added the CD4 count at ART initiation concept to the query for last CD4 count for the patient

--- omod/src/main/java/org/openmrs/module/aijar/fragment/controller/PatientSummaryFragmentController.java
+++ omod/src/main/java/org/openmrs/module/aijar/fragment/controller/PatientSummaryFragmentController.java
@@ -1,7 +1,14 @@
 package org.openmrs.module.aijar.fragment.controller;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Concept;
 import org.openmrs.Obs;
 import org.openmrs.Patient;
 import org.openmrs.Person;
@@ -12,13 +19,6 @@ import org.openmrs.ui.framework.annotation.FragmentParam;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.fragment.FragmentConfiguration;
 import org.openmrs.ui.framework.fragment.FragmentModel;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
 
 /**
  * Created by ssmusoke on 26/01/2016.
@@ -35,7 +35,21 @@ public class PatientSummaryFragmentController {
                            @FragmentParam("patientId") Patient patient) throws ParseException {
 
         Person person = personService.getPerson(patient.getPersonId());
-        List<Obs> cd4Counts = obsService.getLastNObservations(1, person, conceptService.getConcept("5497"), false);
+	    List<Person> who = new ArrayList<Person>();
+	    who.add(person);
+
+	    List<Concept> cd4 = new ArrayList<Concept>();
+	    // Encounter cd4 count
+	    cd4.add(conceptService.getConcept("5497"));
+	    // cd4 at Art initiation
+	    cd4.add(conceptService.getConcept("99082"));
+
+	    List<Concept> currentRegimen = new ArrayList<Concept>();
+	    currentRegimen.add(conceptService.getConcept("90315"));
+
+	    List<Obs> cd4Counts = obsService.getObservations(who, null, cd4, null, null, null, null, 1, null, null, null,
+			    false);
+
         if (cd4Counts.size() > 0) {
             //TODO:use pretty print for date formatting.
             DateFormat formatter = new SimpleDateFormat("dd.MMM.yyyy");
@@ -48,14 +62,13 @@ public class PatientSummaryFragmentController {
             model.addAttribute("lastcd4joiner", "");
         }
 
-
-        List<Obs> currentRegimens = obsService.getLastNObservations(1, person, conceptService.getConcept("90315"), false);
-        if (currentRegimens.size() > 0) {
+	    List<Obs> currentRegimens = obsService.getObservations(who, null, currentRegimen, null, null, null, null, 1, null,
+			    null, null,
+			    false);
+	    if (currentRegimens.size() > 0) {
             model.addAttribute("currentregimen", currentRegimens.get(0).getValueCoded().getName());
         } else {
             model.addAttribute("currentregimen", "None Available");
         }
     }
-
-
 }
