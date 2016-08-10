b34e10b36c4f0fbdbebc5d41337283ab414fb383
lubwamasamuel <lubwamasamuel3@gmail.com>
2016-08-10 18:19:39 +0300

    Fixing errors that were making file not merge the upgrade script well. Adding ability to delete existing  upgrade and new installation sql before a new one is created.

--- tools/src/main/resources/create-sql-scripts.sh
+++ tools/src/main/resources/create-sql-scripts.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+
+# Delete Existing new-install.sql
+rm upgrade/new-install.sql
+# Delete Existing upgrade.sql
+rm upgrade/upgrade.sql
 # Create an SQL script for a new installation
 
 # Turns off foreign key cheks to speed up script execution
-cat database/mysql_script_header.sql > upgrade/new-install.sql
+cat database/mysql_script_header.sql >> upgrade/new-install.sql
 
 # adds the current concept dictionary
 cat database/concept_dictonary_ref.sql >> upgrade/new-install.sql
@@ -19,19 +24,19 @@ cat database/mysql_script_footer.sql >> upgrade/new-install.sql
 # Create an SQL script for an upgrade
 
 # Turns off foreign key cheks to speed up script execution
-cat database/mysql_script_header.sql > upgrade/upgrade.sql
-
-# transform encounters from pre-1.11
-cat database/transform_encounters_from_old_to_new.sql > upgrade/upgrade.sql
+cat database/mysql_script_header.sql >> upgrade/upgrade.sql
 
 # adds the current concept dictionary
 cat database/concept_dictonary_ref.sql >> upgrade/upgrade.sql
 
+# transform encounters from pre-1.11
+cat database/1.6_to_1.11_migration_scripts/transform_encounters_from_old_to_new.sql >> upgrade/upgrade.sql
+
 # Actual data migration from the openmrs_backup to openmrs database
-cat database/1.6_to_1.11_data_transfer.sql >> upgrade/upgrade.sql
+cat database/1.6_to_1.11_migration_scripts/1.6_to_1.11_data_transfer.sql >> upgrade/upgrade.sql
 
 # add visits to encounters - which is required in 1.11.x
-cat database/add_visit_to_encounter.sql >> upgrade/upgrade.sql
+cat database/1.6_to_1.11_migration_scripts/add_visit_to_encounter.sql >> upgrade/upgrade.sql
 
 # adds functions and procedures for BIRT reports
 cat database/enable_birt_report_function.sql >> upgrade/upgrade.sql
