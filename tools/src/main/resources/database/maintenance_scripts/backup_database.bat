2e959a74fd0a0d116e6ae8720a3b564574b7b87b
Stephen S. Musoke <ssmusoke@gmail.com>
2016-08-10 16:08:55 +0300

    Updated the pom file to run different scripts on Windows and on Linux to build the new installation and upgrade scripts

--- tools/src/main/resources/database/maintenance_scripts/backup_database.bat
+++ tools/src/main/resources/database/maintenance_scripts/backup_database.bat
@@ -5,5 +5,4 @@ set _my_datetime=%_my_datetime::=%
 set _my_datetime=%_my_datetime:/=_%
 set _my_datetime=%_my_datetime:.=_%
 
-C:\Program Files\WHO\OpenMRS\mysql-5.0.51a-win32\bin\mysqldump --port=3307 -u root --opt --routines openmrs >
-openmrs%_my_datetime%.sql
+mysqldump -u openmrs -popenmrs --opt --routines openmrs > openmrs%_my_datetime%.sql
