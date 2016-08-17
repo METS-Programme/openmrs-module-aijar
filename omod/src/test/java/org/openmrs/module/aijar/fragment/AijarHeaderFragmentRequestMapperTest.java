5d9b5da9125bdc44823199932dc124337513273c
Stephen S. Musoke <ssmusoke@gmail.com>
2016-08-17 21:29:43 +0300

    Removed include for AijarLoginPageRequestMapper

--- omod/src/test/java/org/openmrs/module/aijar/fragment/AijarHeaderFragmentRequestMapperTest.java
+++ omod/src/test/java/org/openmrs/module/aijar/fragment/AijarHeaderFragmentRequestMapperTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.openmrs.module.aijar.page.AijarLoginPageRequestMapper;
 import org.openmrs.ui.framework.fragment.FragmentRequest;
 
 /**
