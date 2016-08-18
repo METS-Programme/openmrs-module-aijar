de77087316af7e49f1997dc7381e30e3a74c1b71
Charles Olupot <olupotcharles@gmail.com>
2016-08-18 18:28:36 +0300

    Added information to the upgrade script stored procedures to provide feedback on what is happening

--- tools/src/main/resources/database/1.6_to_1.11_migration_scripts/1.6_to_1.11_data_transfer.sql
+++ tools/src/main/resources/database/1.6_to_1.11_migration_scripts/1.6_to_1.11_data_transfer.sql
@@ -28,7 +28,6 @@ DELIMITER $$
 CREATE DEFINER=`openmrs`@`localhost` PROCEDURE `transfer`()
   BEGIN
     DECLARE t_name TEXT;
-    -- DECLARE q_statment TEXT;
     DECLARE t1_columns TEXT;
     DECLARE t2_columns TEXT;
     DECLARE inter_columns TEXT;
@@ -43,59 +42,116 @@ CREATE DEFINER=`openmrs`@`localhost` PROCEDURE `transfer`()
     DECLARE done INT DEFAULT FALSE;
     DECLARE done_primary_keys INT DEFAULT FALSE;
 
+    -- Retrieving all tables from the backup database excluding liquibase tables, form,location,encounter_type which are already populated by aijar
+
     DECLARE cursor_i CURSOR FOR SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'openmrs_backup'  AND table_rows > 0 AND lower(table_name) not like '%liquibase%' AND lower(table_name) not in ('form','location','encounter_type');
+
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
     DROP TEMPORARY TABLE IF EXISTS tb;
     CREATE TEMPORARY TABLE IF NOT EXISTS tb(`name` TEXT);
 
+    -- Loop through all tables of the backup database
+
     OPEN cursor_i;
     read_loop: LOOP
+      -- Working on the current table
       FETCH cursor_i INTO t_name;
+
       IF done THEN
         LEAVE read_loop;
       END IF;
 
+      SELECT CONCAT('Processing ',t_name,' table');
+
+      -- Get column names for table from the backup database
+
+      SELECT CONCAT('Getting columns of ',t_name,' table from the backup database');
+
       SELECT GROUP_CONCAT(COLUMN_NAME) INTO t1_columns FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = t_name AND table_schema = 'openmrs_backup';
+
+      -- Get column names for table from the new database
+
+      SELECT CONCAT('Getting columns of ',t_name,' table from the new database');
 
       SELECT GROUP_CONCAT(COLUMN_NAME) INTO t2_columns FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = t_name AND table_schema = 'openmrs';
 
+      -- Get all primary and keys for table from the old database
+
+      SELECT CONCAT('Getting primary keys for  ',t_name,' table from old database');
+
       SELECT COUNT(*) INTO n FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = t_name AND table_schema = 'openmrs_backup' AND COLUMN_KEY = 'PRI';
+
       SET i=0;
+
       SET where_clause = '';
+
+      SELECT CONCAT('Constructing where clause for table ',t_name,' to join the old database to the new database');
+
       WHILE i < n DO
         SELECT COLUMN_NAME INTO pri_col FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = t_name AND table_schema = 'openmrs_backup' AND COLUMN_KEY = 'PRI' LIMIT i,1;
         SET where_clause = CONCAT(where_clause,'openmrs_backup.',t_name,'.',pri_col,' not in (select ',pri_col,' from openmrs.',t_name,') AND ');
         SET i = i + 1;
       END WHILE;
 
+      -- Finding intersecting columns between the old and the new database for table
+
+      SELECT CONCAT('Finding intersecting columns between the old and the new database for table  ',t_name);
 
       SELECT fn_intersect_string(t1_columns,t2_columns) INTO inter_columns;
 
+      -- Checking if intersecting columns exists
+
       IF(inter_columns is not null AND inter_columns <> '') THEN
+
+        -- For encounters, obs, locations_tag_map, replace the value with 2 as this is the default facility ID
+
         IF t_name in ('encounter','obs','location_tag_map') THEN
           SET inter_columns_insert = REPLACE(inter_columns, 'location_id', '2');
         ELSE
           SET inter_columns_insert = inter_columns;
         END IF;
+
+        -- Creating the data transfer sql from the old database to the new database
+
+        SELECT CONCAT('Creating the data transfer sql statement from the old database to the new database for table  ',t_name);
+
         SET @q_statment = CONCAT('insert into openmrs.',t_name,'(',inter_columns,') select ',inter_columns_insert,' from openmrs_backup.',t_name,if(where_clause <> '',CONCAT(' where ',SUBSTRING(where_clause, 1, CHAR_LENGTH(where_clause) - 4)),''));
+
+        SELECT CONCAT('Data transfer sql statement from the old database to the new database for table  ',t_name, ' creation complete');
+
       END IF;
 
+      SELECT CONCAT('Preparing the data transfer sql statement from the old database to the new database for table  ',t_name);
+
       PREPARE stmt FROM @q_statment;
+
+      SELECT CONCAT('Executing the data transfer sql statement from the old database to the new database for table  ',t_name);
+
       EXECUTE stmt;
+
+      SELECT CONCAT('Data transfer sql statement from the old database to the new database for table  ',t_name, ' execution complete');
+
       DEALLOCATE PREPARE stmt;
 
     END LOOP;
     CLOSE cursor_i;
 
+    SELECT 'Done doing data exports, please wait updating users and providers';
+
     -- Removed the transfer of locations only updating the main location with the values from the old database
+    SELECT 'Updating the location information in the new database with values in the old database';
 
     UPDATE openmrs.location  AS c1, openmrs_backup.location AS c2 SET c1.location_id = c1.location_id, c1.name= c2.name,c1.description = c2.description,c1.address1 = c2.address1,c1.address2 = c2.address2,c1.city_village = c2.city_village,c1.state_province = c2.state_province,c1.postal_code =c1.postal_code,c1.country = c2.country,c1.latitude = c2.latitude,c1.longitude = c2.longitude ,c1.date_created = c2.date_created,c1.county_district = c2.county_district,c1.retired = c2.retired,c1.date_retired =c1.date_retired,c1.retire_reason = c2.retire_reason WHERE c2.location_id = 1  AND c1.location_id = 2;
 
     -- add Provider role to all users with Data Entry and Data Manager Role
     -- Removed condition for role as data manager and data entry because some encounters will not have providers
-    -- @TODO check if user is has ecnouters and then add him as normal provider without data entry privileges
+    -- @TODO check if user is has encounters and then add him as normal provider without data entry privileges
+
+    SELECT 'Exporting providers from old database';
+
     INSERT INTO provider (person_id, creator, date_created, uuid) SELECT person_id, 2, NOW(), UUID() FROM users u WHERE user_id NOT IN (SELECT user_id FROM user_role WHERE role = 'Provider');
+
     INSERT INTO user_role (user_id, role) SELECT user_id, 'Provider' FROM users u WHERE user_id NOT IN (SELECT user_id FROM user_role WHERE role = 'Provider') AND u.user_id IN (SELECT user_id FROM user_role WHERE (role = 'Data Manager' OR role = 'Data Entry'));
 
     -- Check to see if the database encounter table has column provider_id
@@ -124,6 +180,8 @@ BEGIN
 	DROP TEMPORARY TABLE IF EXISTS tb;
 	CREATE TEMPORARY TABLE IF NOT EXISTS tb(`name` TEXT);
 
+  SELECT 'Trying to merge summary pages if there are patients with more than one';
+
 	OPEN cursor_i;
 		read_loop: LOOP
 			FETCH cursor_i INTO patient,ecnounter,total;
@@ -150,3 +208,5 @@ DELIMITER ;
 
 call transfer();
 call mergeSummaryPages();
+
+SELECT 'Database export complete';
