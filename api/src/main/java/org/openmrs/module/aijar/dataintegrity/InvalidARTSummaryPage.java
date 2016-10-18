6cf1eec71be267e006a33a85fe853f27f9111060
Stephen S. Musoke <ssmusoke@gmail.com>
2016-10-18 20:53:53 +0300

    Merged the rule result transformation methods into the individual rule execution methods Added @Component annotation so that the rules can be discovered by tthe DataIntegrity module

--- api/src/main/java/org/openmrs/module/aijar/dataintegrity/InvalidARTSummaryPage.java
+++ api/src/main/java/org/openmrs/module/aijar/dataintegrity/InvalidARTSummaryPage.java
@@ -6,12 +6,12 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.Query;
-import org.hibernate.SQLQuery;
 import org.hibernate.SessionFactory;
 import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
-import org.openmrs.module.dataintegrity.rule.RuleDefn;
+import org.openmrs.module.dataintegrity.db.DataIntegrityRule;
 import org.openmrs.module.dataintegrity.rule.RuleResult;
+import org.springframework.stereotype.Component;
 
 /**
  * Data integrity rules for the ART Summary page which are:
@@ -20,7 +20,8 @@ import org.openmrs.module.dataintegrity.rule.RuleResult;
  *  <li>Patients With More Than One Summary Page</li>
  * </ol>
  */
-public class InvalidARTSummaryPage implements RuleDefn<Patient>  {
+@Component
+public class InvalidARTSummaryPage extends BasePatientRuleDefinition {
 	
 	private Log log = LogFactory.getLog(getClass());
 	
@@ -33,59 +34,66 @@ public class InvalidARTSummaryPage implements RuleDefn<Patient>  {
 		return ruleResults;
 	}
 	
+	/**
+	 * Patients with more than one ART Summary Page
+	 * @return
+	 */
 	private List<RuleResult<Patient>> patientsWithMoreThanOneSummaryARTPage() {
 		String queryString = "SELECT patient FROM Encounter e WHERE e.voided = false AND e.encounterType.uuid = '8d5b27bc-c2cc-11de-8d13-0010c6dffd0f' GROUP BY e.patient.patientId HAVING COUNT(e.patient.patientId) > 1";
 		
 		Query query = getSessionFactory().openSession().createQuery(queryString);
-		log.debug("Patients with encounters and no summary page query " + query.getQueryString());
 		
 		List<Patient> patientList = query.list();
 		
-		return patientToRuleResultTransformerForpatientsWithMoreThanOneSummaryPage(patientList, "More than one ART Summary Page");
+		List<RuleResult<Patient>> ruleResults = new ArrayList<>();
+		for (Patient patient : patientList) {
+			RuleResult<Patient> ruleResult = new RuleResult<>();
+			ruleResult.setActionUrl("coreapps/patientdashboard/patientDashboard.page?patientId=" + patient.getUuid());
+			ruleResult.setNotes("Client# " + getHIVClinicNumber(patient) + ", more than one ART Summary Page");
+			ruleResult.setEntity(patient);
+			
+			ruleResults.add(ruleResult);
+		}
+		
+		return ruleResults;
 	}
 	
+	/**
+	 * Patients with ART Encounter and Health Education encounters but no ART summary page
+	 * @return
+	 */
 	private List<RuleResult<Patient>> patientsWithEncountersAndNOSummaryPage() {
 		String queryString = "SELECT patient FROM Encounter e WHERE e.voided = false AND ( e.encounterType.uuid = '8d5b2be0-c2cc-11de-8d13-0010c6dffd0f' OR e.encounterType.uuid = '6d88e370-f2ba-476b-bf1b-d8eaf3b1b67e') AND e.patient.patientId NOT IN (SELECT ee.patient.patientId FROM Encounter ee WHERE ee.encounterType.uuid = '8d5b27bc-c2cc-11de-8d13-0010c6dffd0f') GROUP BY e.patient.patientId";
 		
 		Query query = getSessionFactory().openSession().createQuery(queryString);
-		log.debug("Patients with encounters and no summary page query " + query.getQueryString());
 		
 		List<Patient> patientList = query.list();
 		
-		return patientToRuleResultTransformerForPatientWithNoSummaryPage(patientList, "Has encounters with No ART Summary Page");
+		List<RuleResult<Patient>> ruleResults = new ArrayList<>();
+		for (Patient patient : patientList) {
+			RuleResult<Patient> ruleResult = new RuleResult<>();
+			ruleResult.setActionUrl("coreapps/patientdashboard/patientDashboard.page?patientId=" + patient.getUuid());
+			ruleResult.setNotes("Client# " + getHIVClinicNumber(patient) + ", no ART Summary Page");
+			ruleResult.setEntity(patient);
+			
+			ruleResults.add(ruleResult);
+		}
+		
+		return ruleResults;
 	}
 	
 	private SessionFactory getSessionFactory() {
 		return Context.getRegisteredComponent("sessionFactory", SessionFactory.class);
 	}
 	
-	private List<RuleResult<Patient>> patientToRuleResultTransformerForpatientsWithMoreThanOneSummaryPage(List<Patient> patients, String notes) {
-		
-		List<RuleResult<Patient>> ruleResults = new ArrayList<>();
-		for (Patient patient : patients) {
-			RuleResult<Patient> ruleResult = new RuleResult<>();
-			ruleResult.setActionUrl("coreapps/patientdashboard/patientDashboard.page?patientId=" + patient.getUuid());
-			ruleResult.setNotes(notes);
-			ruleResult.setEntity(patient);
-			
-			ruleResults.add(ruleResult);
-		}
-		
-		return ruleResults;
-	}
-	
-	private List<RuleResult<Patient>> patientToRuleResultTransformerForPatientWithNoSummaryPage(List<Patient> patients, String notes) {
-		
-		List<RuleResult<Patient>> ruleResults = new ArrayList<>();
-		for (Patient patient : patients) {
-			RuleResult<Patient> ruleResult = new RuleResult<>();
-			ruleResult.setActionUrl("coreapps/clinicianfacing/patient.page?patientId=" + patient.getUuid());
-			ruleResult.setNotes(notes);
-			ruleResult.setEntity(patient);
-			
-			ruleResults.add(ruleResult);
-		}
-		
-		return ruleResults;
+	@Override
+	public DataIntegrityRule getRule() {
+		DataIntegrityRule rule = new DataIntegrityRule();
+		rule.setRuleCategory("patient");
+		rule.setHandlerConfig("java");
+		rule.setHandlerClassname(getClass().getName());
+		rule.setRuleName("Invalid ART Summary Pages");
+		rule.setUuid("c57c3b5a-9019-11e6-85aa-b75ca9392202");
+		return rule;
 	}
 }
