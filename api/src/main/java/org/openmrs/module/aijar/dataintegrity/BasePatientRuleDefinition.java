a1d87f9bbb84854e9128e675fea64ca32f93a4d6
Stephen S. Musoke <ssmusoke@gmail.com>
2016-11-01 15:09:47 +0300

    Added exposed infant number function, session method and logging that can be used by subclasses

--- api/src/main/java/org/openmrs/module/aijar/dataintegrity/BasePatientRuleDefinition.java
+++ api/src/main/java/org/openmrs/module/aijar/dataintegrity/BasePatientRuleDefinition.java
@@ -1,5 +1,9 @@
 package org.openmrs.module.aijar.dataintegrity;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.openmrs.Patient;
 import org.openmrs.PatientIdentifierType;
 import org.openmrs.api.PatientService;
@@ -12,6 +16,7 @@ import org.openmrs.module.dataintegrity.rule.RuleDefinition;
  */
 public abstract class BasePatientRuleDefinition implements RuleDefinition<Patient> {
 	
+	protected Log log = LogFactory.getLog(getClass());
 	/**
 	 * Return the HIV Clinic number for the patient
 	 * @param patient
@@ -21,5 +26,15 @@ public abstract class BasePatientRuleDefinition implements RuleDefinition<Patien
 		PatientService patientService = Context.getPatientService();
 		PatientIdentifierType pit = patientService.getPatientIdentifierTypeByUuid(PatientIdentifierTypes.HIV_CARE_NUMBER.uuid());
 		 return patient.getPatientIdentifier(pit).toString();
+	}
+	
+	public String getExposedInfantNumber(Patient patient) {
+		PatientService patientService = Context.getPatientService();
+		PatientIdentifierType pit = patientService.getPatientIdentifierTypeByUuid(PatientIdentifierTypes.EXPOSED_INFANT_NUMBER.uuid());
+		return patient.getPatientIdentifier(pit).toString();
+	}
+	
+	public Session getSession() {
+		return Context.getRegisteredComponent("sessionFactory", SessionFactory.class).getCurrentSession();
 	}
 }
