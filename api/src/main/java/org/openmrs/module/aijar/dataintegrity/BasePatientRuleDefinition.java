d503c8568fd0f9643ee264928a7d75d26b135844
Stephen S. Musoke <ssmusoke@gmail.com>
2016-11-02 17:21:52 +0300

    Validation rules for ART Encounters and Information

--- api/src/main/java/org/openmrs/module/aijar/dataintegrity/BasePatientRuleDefinition.java
+++ api/src/main/java/org/openmrs/module/aijar/dataintegrity/BasePatientRuleDefinition.java
@@ -1,5 +1,7 @@
 package org.openmrs.module.aijar.dataintegrity;
 
+import java.text.DateFormat;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.Session;
@@ -17,6 +19,7 @@ import org.openmrs.module.dataintegrity.rule.RuleDefinition;
 public abstract class BasePatientRuleDefinition implements RuleDefinition<Patient> {
 	
 	protected Log log = LogFactory.getLog(getClass());
+	
 	/**
 	 * Return the HIV Clinic number for the patient
 	 * @param patient
@@ -28,13 +31,30 @@ public abstract class BasePatientRuleDefinition implements RuleDefinition<Patien
 		 return patient.getPatientIdentifier(pit).toString();
 	}
 	
+	/**
+	 * Exposed Infant number for the patient
+	 * @param patient
+	 * @return
+	 */
 	public String getExposedInfantNumber(Patient patient) {
 		PatientService patientService = Context.getPatientService();
 		PatientIdentifierType pit = patientService.getPatientIdentifierTypeByUuid(PatientIdentifierTypes.EXPOSED_INFANT_NUMBER.uuid());
 		return patient.getPatientIdentifier(pit).toString();
 	}
 	
+	/**
+	 * A Session instance used by sub-classes
+	 * @return
+	 */
 	public Session getSession() {
 		return Context.getRegisteredComponent("sessionFactory", SessionFactory.class).getCurrentSession();
+	}
+	
+	/**
+	 * A formatter for dates
+	 * @return
+	 */
+	public DateFormat getDateFormatter() {
+		return Context.getDateFormat();
 	}
 }
