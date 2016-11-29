054c574440fcb50ca5bed8296fb15c65f8f89521
Charles Olupot <olupotcharles@gmail.com>
2016-11-29 10:36:34 +0300

    Added the Finger Print patient Attribute Type Added the Finger Print on Registration

--- api/src/main/java/org/openmrs/module/aijar/metadata/core/PersonAttributeTypes.java
+++ api/src/main/java/org/openmrs/module/aijar/metadata/core/PersonAttributeTypes.java
@@ -83,46 +83,67 @@ public class PersonAttributeTypes {
             return "8d872150-c2cc-11de-8d13-0010c6dffd0f";
         }
     };
-    
+
     public static PersonAttributeTypeDescriptor TELEPHONE_NUMBER_2 = new PersonAttributeTypeDescriptor() {
         @Override
         public double sortWeight() {
             return 8;
         }
-        
+
         @Override
         public String name() {
             return "Alternate Telephone Number";
         }
-        
+
         @Override
         public String description() {
             return "Alternate Telephone number";
         }
-        
+
         public String uuid() {
             return "8c44d411-285f-46c6-9f17-c2f919823b34";
         }
     };
-    
+
     public static PersonAttributeTypeDescriptor TELEPHONE_NUMBER_3 = new PersonAttributeTypeDescriptor() {
         @Override
         public double sortWeight() {
             return 10;
         }
-        
+
         @Override
         public String name() {
             return "Second Alternate Telephone Number";
         }
-        
+
         @Override
         public String description() {
             return "Second Alternate Telephone number";
         }
-        
+
         public String uuid() {
             return "a00eda65-2f66-4fda-a683-c1787eb626a9";
+        }
+    };
+
+    public static PersonAttributeTypeDescriptor FINGER_PRINT = new PersonAttributeTypeDescriptor() {
+        @Override
+        public double sortWeight() {
+            return 12;
+        }
+
+        @Override
+        public String name() {
+            return "FingerPrint";
+        }
+
+        @Override
+        public String description() {
+            return "Person Fingerprint";
+        }
+
+        public String uuid() {
+            return "a41339f9-5014-45f4-91d6-bab84c6c62f1";
         }
     };
 }
