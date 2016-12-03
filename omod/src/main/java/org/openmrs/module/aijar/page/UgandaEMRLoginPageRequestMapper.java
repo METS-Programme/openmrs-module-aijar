7f58f54113fe77630449578dcf16af7e507a6342
Stephen S. Musoke <ssmusoke@gmail.com>
2016-12-03 09:35:08 +0300

    Cleanup of unnecessary constructor

--- omod/src/main/java/org/openmrs/module/aijar/page/UgandaEMRLoginPageRequestMapper.java
+++ omod/src/main/java/org/openmrs/module/aijar/page/UgandaEMRLoginPageRequestMapper.java
@@ -15,11 +15,6 @@ import org.springframework.stereotype.Component;
 public class UgandaEMRLoginPageRequestMapper implements PageRequestMapper {
 
 	protected final Log log = LogFactory.getLog(getClass());
-	
-	public UgandaEMRLoginPageRequestMapper(){
-		log.info("initializing XXX " + getClass());
-	}
-
 	/**
 	 * Implementations should call {@link PageRequest#setProviderNameOverride(String)} and
 	 * {@link PageRequest#setPageNameOverride(String)}, and return true if they want to remap a request,
