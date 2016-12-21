e1e1b88aca0a2aa9e21a390205ff0856ff3d4e59
Lubwama Samuel <lubwamasamuel3@gmail.com>
2016-12-21 13:21:07 +0300

    mark paient dead with test
    improving pom file dependencies to enable extention of BaseModuleWebContextSesitiveTest class
    
    adding all test areas
    
    putting back plugin dependency
    
    adding enable disable for mark patient dead
    
    Add link to show details of an encounter on the patient dashboard
    
    Add telephone number and marital status to patient search widget
    
    Adding ability to build the distro during the package goal
    Update the pom.xml to include all versions for the distro.properties to provide a single location for managing the module versions
    
    Increased the HTML Form Entry UI version to 1.6.2-SNAPSHOT to show the view HTML Form link on the visits dashboard
    
    Added batchAnswers parameter to disable interactive mode
    
    Increased the SDK version to the latest - 3.6.1
    
    Moved SDK installation to travis.yml file and removed the batchAnswers option from build-distro goal
    
    Test for using batchAnswers and param tag without installing the plugin via travis.yml file
    
    Changing Location of Mark Patient Dead Js
    
    resolving conflicts and merge changes
    
    Resolving Issues that break Travis
    
    Adding Checks to make sure death date is not after today's date and not before date of birth
    
    checking senarios for death date greater than today and death date less than date of birth
    
    modify date picker eliminate dates before birth and after current date
    
    modify date picker eliminate dates before birth and after current date
    
    add checks to ensure death date is not greater than today or less than birth date
    
    reaction to comment
    
    removing Person Dependency and extention of PatientService Impli
    
    Chaning from "null" to ""
    
    Changing person attribute to patient
    
    Removing .getPerson() in Controller
    Changing log from system.out.print
    
    changing return fron null to redirect to different pages basing on failing scenario
    
    changing tests to meet the return on fail

---
+++ omod/src/test/java/org/openmrs/module/aijar/page/controller/MarkPatientDeadTest.java
@@ -0,0 +1,105 @@
+package org.openmrs.module.aijar.page.controller;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.Concept;
+import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
+import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by lubwamasamuel on 19/15/2016.
+ */
+public class MarkPatientDeadTest extends BaseModuleWebContextSensitiveTest {
+
+    Patient patient = new Patient();
+    Concept concept = new Concept();
+    Date date = new Date();
+
+
+    @Before
+    public void setRequirementsForTest() {
+        patient = Context.getPatientService().getPatient(2);
+        concept = Context.getConceptService().getConcept("unknown");
+    }
+
+
+    private Date modifyPatientBirthDate(int daysOff,Date date){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DATE, daysOff);
+        Date modifiedDated = cal.getTime();
+        return modifiedDated;
+    }
+
+
+    /**
+     * This tests if the patient is marked as dead when given the right params
+     */
+    @Test
+    public void shouldMarkPatientDeadSuccessfully() {
+        MarkPatientDeadPageController markPatientDeadPageController = new MarkPatientDeadPageController();
+        markPatientDeadPageController.post(concept.getUuid(), true, date, patient.getUuid().toString());
+        Assert.assertEquals(patient.getDead(), true);
+        Assert.assertEquals(patient.getCauseOfDeath(), concept);
+    }
+
+    /**
+     * This tests if the patient is marked as dead when date is null
+     */
+    @Test
+    public void shouldNotMarkPatientDeadWhenDateIsNull() {
+        MarkPatientDeadPageController markPatientDeadPageController = new MarkPatientDeadPageController();
+        markPatientDeadPageController.post(concept.getUuid(), true, null, patient.getUuid().toString());
+        Assert.assertEquals(patient.getDead(), false);
+        Assert.assertNotEquals(patient.getCauseOfDeath(), concept);
+    }
+
+
+    /**
+     * This tests if the patient is marked as dead when cause of death is null
+     */
+    @Test
+    public void shouldNotMarkPatientDeadWhenCauseOfDeathIsNull() {
+        MarkPatientDeadPageController markPatientDeadPageController = new MarkPatientDeadPageController();
+        markPatientDeadPageController.post("", true, date, patient.getUuid().toString());
+        Assert.assertEquals(patient.getDead(), false);
+        Assert.assertNotEquals(patient.getCauseOfDeath(), concept);
+    }
+
+    /**
+     * This tests if the patient is marked as dead when checkbox dead is not checked ie false
+     */
+    @Test
+    public void shouldNotMarkPatientDeadWhenDeadIsFalse() {
+        MarkPatientDeadPageController markPatientDeadPageController = new MarkPatientDeadPageController();
+        markPatientDeadPageController.post(concept.getUuid(), false, date, patient.getUuid().toString());
+        Assert.assertEquals(patient.getDead(), false);
+        Assert.assertNotEquals(patient.getCauseOfDeath(), concept);
+    }
+
+
+
+    /**
+     * This tests if the patient is marked as dead when patient is null
+     */
+    @Test
+    public void shouldNotMarkPatientDeadWhenDPatientIsNull() {
+        MarkPatientDeadPageController markPatientDeadPageController = new MarkPatientDeadPageController();
+        Assert.assertEquals(markPatientDeadPageController.post("", false, null, null),"redirect:/referenceapplication/home.page");
+    }
+
+
+    /**
+     * This tests if the patient is marked as dead when checkbox dead is not checked ie false
+     */
+    @Test
+    public void shouldNotMarkPatientDeadWhenNoParameter() {
+        MarkPatientDeadPageController markPatientDeadPageController = new MarkPatientDeadPageController();
+        Assert.assertEquals(markPatientDeadPageController.post("", false, null, null),"redirect:/referenceapplication/home.page");
+    }
+}
