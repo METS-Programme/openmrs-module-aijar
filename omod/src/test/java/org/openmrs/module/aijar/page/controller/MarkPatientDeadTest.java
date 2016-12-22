570076b07a83ef58986805a5a1cbb5cedb37bcec
Lubwama Samuel <lubwamasamuel3@gmail.com>
2016-12-22 12:15:14 +0300

    adding all test areas

--- omod/src/test/java/org/openmrs/module/aijar/page/controller/MarkPatientDeadTest.java
+++ omod/src/test/java/org/openmrs/module/aijar/page/controller/MarkPatientDeadTest.java
@@ -1,35 +1,95 @@
 package org.openmrs.module.aijar.page.controller;
 
-import java.util.Date;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.Concept;
 import org.openmrs.Patient;
-import org.openmrs.api.ConceptService;
-import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Context;
-import org.openmrs.api.impl.PatientServiceImpl;
 import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
-import org.openmrs.web.test.BaseWebContextSensitiveTest;
+
+import java.util.Date;
 
 /**
  * Created by lubwamasamuel on 19/15/2016.
  */
 public class MarkPatientDeadTest extends BaseModuleWebContextSensitiveTest {
 
+    Patient patient = new Patient();
+    Concept concept = new Concept();
+    Date date = new Date();
 
+
+    @Before
+    public void setRequirementsForTest() {
+        patient = Context.getPatientService().getPatient(2);
+        concept = Context.getConceptService().getConcept("unknown");
+    }
+
+
+    /**
+     * This tests if the patient is marked as dead when given the right params
+     */
     @Test
     public void shouldMarkPatientDeadSuccessfully() {
         MarkPatientDeadPageController markPatientDeadPageController = new MarkPatientDeadPageController();
-        Date date = new Date();
-        PatientService patientService = Context.getPatientService();
-        Patient patient = new Patient();
-        Concept concept=new Concept();
-        concept=Context.getConceptService().getConcept("unknown");
-        patient = patientService.getPatient(2);
         markPatientDeadPageController.post(concept.getUuid(), true, date, patient.getUuid().toString());
-        assert patient.getDead().equals(true);
-        assert patient.getCauseOfDeath().equals(concept);
+        Assert.assertEquals(patient.getDead(), true);
+        Assert.assertEquals(patient.getCauseOfDeath(), concept);
+    }
+
+    /**
+     * This tests if the patient is marked as dead when date is null
+     */
+    @Test
+    public void shouldNotMarkPatientDeadWhenDateIsNull() {
+        MarkPatientDeadPageController markPatientDeadPageController = new MarkPatientDeadPageController();
+        markPatientDeadPageController.post(concept.getUuid(), true, null, patient.getUuid().toString());
+        Assert.assertEquals(patient.getDead(), false);
+        Assert.assertNotEquals(patient.getCauseOfDeath(), concept);
+    }
+
+
+    /**
+     * This tests if the patient is marked as dead when cause of death is null
+     */
+    @Test
+    public void shouldNotMarkPatientDeadWhenCauseOfDeathIsNull() {
+        MarkPatientDeadPageController markPatientDeadPageController = new MarkPatientDeadPageController();
+        markPatientDeadPageController.post("null", true, date, patient.getUuid().toString());
+        Assert.assertEquals(patient.getDead(), false);
+        Assert.assertNotEquals(patient.getCauseOfDeath(), concept);
+    }
+
+    /**
+     * This tests if the patient is marked as dead when checkbox dead is not checked ie false
+     */
+    @Test
+    public void shouldNotMarkPatientDeadWhenDeadIsFalse() {
+        MarkPatientDeadPageController markPatientDeadPageController = new MarkPatientDeadPageController();
+        markPatientDeadPageController.post(concept.getUuid(), false, date, patient.getUuid().toString());
+        Assert.assertEquals(patient.getDead(), false);
+        Assert.assertNotEquals(patient.getCauseOfDeath(), concept);
+    }
+
+
+
+    /**
+     * This tests if the patient is marked as dead when patient is null
+     */
+    @Test
+    public void shouldNotMarkPatientDeadWhenDPatientIsNull() {
+        MarkPatientDeadPageController markPatientDeadPageController = new MarkPatientDeadPageController();
+        Assert.assertEquals(markPatientDeadPageController.post("null", false, null, null),null);
+    }
+
+
+    /**
+     * This tests if the patient is marked as dead when checkbox dead is not checked ie false
+     */
+    @Test
+    public void shouldNotMarkPatientDeadWhenNoParameter() {
+        MarkPatientDeadPageController markPatientDeadPageController = new MarkPatientDeadPageController();
+        Assert.assertEquals(markPatientDeadPageController.post("null", false, null, null),null);
     }
 }
