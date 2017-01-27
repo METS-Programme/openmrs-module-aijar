4d5c69be5aba003d304c6957446795979df71d37
slubwama <lubwamasamuel3@gmail.com>
2017-01-27 10:48:39 +0300

    Revert "Patient as deceased"

--- omod/src/main/java/org/openmrs/module/aijar/page/controller/MarkPatientDeadPageController.java
+++ omod/src/main/java/org/openmrs/module/aijar/page/controller/MarkPatientDeadPageController.java
@@ -1,7 +1,5 @@
 package org.openmrs.module.aijar.page.controller;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.openmrs.Concept;
 import org.openmrs.ConceptAnswer;
 import org.openmrs.Patient;
@@ -9,7 +7,6 @@ import org.openmrs.Person;
 import org.openmrs.api.PatientService;
 import org.openmrs.api.PersonService;
 import org.openmrs.api.context.Context;
-import org.openmrs.api.impl.PatientServiceImpl;
 import org.openmrs.module.appui.UiSessionContext;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.annotation.SpringBean;
@@ -24,18 +21,18 @@ import java.util.regex.Pattern;
  * Created by lubwamasamuel on 15/06/2016.
  */
 public class MarkPatientDeadPageController {
-    protected final Log log = LogFactory.getLog(this.getClass());
-
     public void controller(UiSessionContext sessionContext, PageModel model) {
     }
 
-    public void get(@SpringBean PageModel pageModel, @RequestParam(value = "breadcrumbOverride", required = false) String breadcrumbOverride, @RequestParam("patientId") String patientId) {
+    public void get(@SpringBean PageModel pageModel, @RequestParam(value = "breadcrumbOverride",required = false) String breadcrumbOverride, @RequestParam("patientId") String patientId) {
+        PersonService personService = Context.getPersonService();
         PatientService patientService = Context.getPatientService();
 
         String conceptId = Context.getAdministrationService().getGlobalProperty("concept.causeOfDeath");
 
         Patient patient = patientService.getPatientByUuid(patientId);
-        pageModel.put("birthDate", patient.getBirthdate());
+        Person person = personService.getPerson(patient.getPatientId());
+        pageModel.put("person", person);
         pageModel.put("patient", patient);
         pageModel.put("patientId", patientId);
         pageModel.put("breadcrumbOverride", breadcrumbOverride);
@@ -44,29 +41,28 @@ public class MarkPatientDeadPageController {
         }
     }
 
-    public String post(@RequestParam(value = "causeOfDeath", required = false) String causeOfDeath, @RequestParam(value = "dead", required = false) Boolean dead, @RequestParam(value = "deathDate", required = false) Date deathDate, @RequestParam("patientId") String patientId) {
-
-
-        PatientService patientService = Context.getPatientService();
-        Patient patient = patientService.getPatientByUuid(patientId);
+    public String post(@RequestParam(value = "causeOfDeath",required = false) String causeOfDeath, @RequestParam(value = "dead",required = false) Boolean dead, @RequestParam(value = "deathDate",required = false) Date deathDate, @RequestParam("patientId") String patientId, UiUtils
+            uiUtils) {
+        PersonService personService = Context.getPersonService();
         try {
-            Date date = new Date();
-
-            if (dead != null && !causeOfDeath.equals("") && deathDate != null && !deathDate.before(patient.getBirthdate()) && !deathDate.after(date)) {
-                patient.setDead(dead);
-                patient.setCauseOfDeath(getConceptByUUId(causeOfDeath));
-                patient.setDeathDate(deathDate);
+            PatientService patientService = Context.getPatientService();
+            Patient patient = patientService.getPatientByUuid(patientId);
+            Person person = personService.getPerson(patient.getPatientId());
+            if (dead!=null && causeOfDeath != null && deathDate != null) {
+                person.setDead(dead);
+                person.setCauseOfDeath(getConceptByUUId(causeOfDeath));
+                person.setDeathDate(deathDate);
             } else {
-                patient.setDeathDate(null);
-                patient.setDead(false);
-                patient.setCauseOfDeath(null);
+                person.setDeathDate(null);
+                person.setDead(false);
+                person.setCauseOfDeath(null);
             }
 
-            patientService.savePatient(patient);
+            personService.savePerson(person);
             return "redirect:/coreapps/clinicianfacing/patient.page?patientId=" + patient.getUuid() + "";
         } catch (Exception e) {
-            log.error(e.getMessage());
-                return "redirect:/aijar/markPatientDead.page?patientId={{" + patient.getUuid() + "}}";
+            System.out.println(e);
+            return null;
         }
 
     }
@@ -79,4 +75,5 @@ public class MarkPatientDeadPageController {
         Concept concept = Context.getConceptService().getConcept(conceptId);
         return concept.getAnswers();
     }
+
 }
--- omod/src/main/webapp/resources/js/aijar.js
+++ omod/src/main/webapp/resources/js/aijar.js
@@ -45,23 +45,3 @@ function changeFieldDateToJavascriptDate(dateValue) {
 }
 
 
-/*
-* Hide the container, and disable all elements in it
-*
-* @param the Id of the container
-*/
-function hideContainer(container) {
-    jq(container).addClass('hidden');
-    jq(container + ' :input').attr('disabled', true);
-    jq(container + ' :input').prop('checked', false);
-}
-/*
- * Show the container, and enable all elements in it
- *
- * @param the Id of the container
- */
-function showContainer(container) {
-    jq(container).removeClass('hidden');
-    jq(container + ' :input').attr('disabled', false);
-    jq(container + ' :input').prop('checked', false);
-}
