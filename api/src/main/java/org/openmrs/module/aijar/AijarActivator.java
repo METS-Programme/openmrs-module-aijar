a12ab0e00e9cab81b3f61416a50d038a355b660e
ningosi <nicotwendelee@gmail.com>
2017-03-17 11:19:28 +0300

    Added program xml into the activator for them to load on start

--- api/src/main/java/org/openmrs/module/aijar/AijarActivator.java
+++ api/src/main/java/org/openmrs/module/aijar/AijarActivator.java
@@ -118,6 +118,8 @@ public class AijarActivator extends org.openmrs.module.BaseModuleActivator {
             log.info("TB concepts imported");
             dataImporter.importData("metadata/Concepts_OPD-1.xml");
             log.info("OPD concepts imported");
+            dataImporter.importData("metadata/Concepts_Programs-1.xml");
+            log.info("Programs concepts imported");
 
             // save defined global properties
             administrationService.saveGlobalProperties(configureGlobalProperties());
