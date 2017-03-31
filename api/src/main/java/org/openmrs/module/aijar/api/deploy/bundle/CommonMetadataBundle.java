702c678f8983a98f97d2dd6c4ec5abd9839cbc8b
slubwama <lubwamasamuel3@gmail.com>
2017-03-31 19:37:12 +0300

    Smc card issues 1.0.14 (#178)
    Fix issues on SMC form deployed in UgandaEMR 1.0.14

--- api/src/main/java/org/openmrs/module/aijar/api/deploy/bundle/CommonMetadataBundle.java
+++ api/src/main/java/org/openmrs/module/aijar/api/deploy/bundle/CommonMetadataBundle.java
@@ -1,10 +1,8 @@
 package org.openmrs.module.aijar.api.deploy.bundle;
 
-import org.openmrs.module.aijar.metadata.core.EncounterTypes;
-import org.openmrs.module.aijar.metadata.core.PatientIdentifierTypes;
-import org.openmrs.module.aijar.metadata.core.PersonAttributeTypes;
-import org.openmrs.module.aijar.metadata.core.Roles;
+import org.openmrs.module.aijar.metadata.core.*;
 import org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle;
+import static org.openmrs.module.metadatadeploy.bundle.CoreConstructors.encounterRole;
 import org.springframework.stereotype.Component;
 
 /**
@@ -52,8 +50,11 @@ public class CommonMetadataBundle extends AbstractMetadataBundle {
         install(Roles.MID_WIFE);
         log.info("Roles installed");
 
+
         //Install Encounter Type
         log.info("Installing EncounterTypes");
         install(EncounterTypes.PNC_ENCOUNTER_TYPE);
+        install(EncounterTypes.SMC_FOLLOW_UP_ENCOUNTER);
+        install(encounterRole(EncounterRoles.ASSISTANT_CIRCUMCISER_NAME,EncounterRoles.ASSISTANT_CIRCUMCISER_DESCRIPTION,EncounterRoles.ASSISTANT_CIRCUMCISER_UUID));
     }
 }
