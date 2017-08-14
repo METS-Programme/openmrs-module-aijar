3fa360ed3f2204e9976f7a0d63989ae3936a9872
Kenneth Omondi Ochieng <omondi.ken@gmail.com>
2017-08-14 17:47:10 +0300

    Added Data Integrity checks for TB Encounters (#286)

--- api/src/main/java/org/openmrs/module/aijar/AijarConstants.java
+++ api/src/main/java/org/openmrs/module/aijar/AijarConstants.java
@@ -14,4 +14,13 @@ public class AijarConstants {
      * Global property names
      */
     public static final String GP_HEALTH_CENTER_NAME = MODULE_ID + ".healthCenterName";
+    
+    /**
+     * Concepts
+     */
+    public static final String UNIT_TB_NUMBER = "304df0d0-afe4-4a61-a917-d684b100a65a";
+    
+    public static final String HSD_TB_NUMBER = "d1cda288-4853-4450-afbc-76bd4e65ea70";
+    
+    public static final String DISTRICT_TB_NUMBER = "67e9ec2f-4c72-408b-8122-3706909d77ec";
 }
