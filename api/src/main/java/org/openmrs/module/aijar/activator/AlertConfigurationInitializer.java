14940b6f5e71614f88c451a479bb591a3a26d078
Lubwama Samuel <lubwamasamuel3@gmail.com>
2017-09-18 08:16:06 +0300

    Creating private method to remove the statements

--- api/src/main/java/org/openmrs/module/aijar/activator/AlertConfigurationInitializer.java
+++ api/src/main/java/org/openmrs/module/aijar/activator/AlertConfigurationInitializer.java
@@ -11,32 +11,37 @@ import static org.openmrs.module.aijar.AijarConstants.*;
 import static org.openmrs.module.aijar.AijarConstants.GP_DHIS2_DEFAULT_ALERT_MESSAGE;
 import static org.openmrs.module.aijar.AijarConstants.GP_DHIS2_VALUE;
 
-public class AlertConfigurationInitializer implements Initializer{
+public class AlertConfigurationInitializer implements Initializer {
     protected Log log = LogFactory.getLog(AlertConfigurationInitializer.class);
+
     @Override
     public void started() {
-        AdministrationService administrationService= org.openmrs.api.context.Context.getAdministrationService();
-        AijarService aijarService= org.openmrs.api.context.Context.getService(AijarService.class);
-        // set alert if National Health Provider Identifier is not set
-        if (administrationService.getGlobalProperty(GP_NHPI).equalsIgnoreCase(GP_NHPI_VALUE)) {
-            aijarService.setAlertForAllUsers(GP_NHPI_DEFAULT_ALERT_MESSAGE);
-            log.info("Creating alert "+GP_NHPI_DEFAULT_ALERT_MESSAGE);
-        }
 
+
+        // set alert if National Health Provider Identifier is not set
+        createAlertWhenDefaultIsNotSet(GP_NHPI, GP_NHPI_VALUE, GP_NHPI_DEFAULT_ALERT_MESSAGE);
         // set alert if Health Center Name is not set
-        if (administrationService.getGlobalProperty(GP_HEALTH_CENTER_NAME).equalsIgnoreCase(GP_HEALTH_CENTER_NAME_VALUE)) {
-            aijarService.setAlertForAllUsers(GP_HEALTH_CENTER_NAME_DEFAULT_ALERT_MESSAGE);
-            log.info("Creating alert "+GP_HEALTH_CENTER_NAME_DEFAULT_ALERT_MESSAGE);
-        }
+        createAlertWhenDefaultIsNotSet(GP_HEALTH_CENTER_NAME, GP_HEALTH_CENTER_NAME_VALUE, GP_HEALTH_CENTER_NAME_DEFAULT_ALERT_MESSAGE);
         // set alert if DHIS2 Organization UUID is not set
-        if (administrationService.getGlobalProperty(GP_DHIS2).equalsIgnoreCase(GP_DHIS2_VALUE)) {
-            aijarService.setAlertForAllUsers(GP_DHIS2_DEFAULT_ALERT_MESSAGE);
-            log.info("Creating alert "+GP_DHIS2_DEFAULT_ALERT_MESSAGE);
-        }
+        createAlertWhenDefaultIsNotSet(GP_DHIS2, GP_DHIS2_VALUE, GP_DHIS2_DEFAULT_ALERT_MESSAGE);
     }
 
     @Override
     public void stopped() {
 
+    }
+
+    /**
+     * @param globalPropertyName
+     * @param globalPropertyDefault
+     * @param messageAlert
+     */
+    private void createAlertWhenDefaultIsNotSet(String globalPropertyName, String globalPropertyDefault, String messageAlert) {
+        AijarService aijarService = org.openmrs.api.context.Context.getService(AijarService.class);
+        AdministrationService administrationService = org.openmrs.api.context.Context.getAdministrationService();
+        if (administrationService.getGlobalProperty(globalPropertyName).equalsIgnoreCase(globalPropertyDefault)) {
+            aijarService.setAlertForAllUsers(messageAlert);
+            log.info("Creating alert " + messageAlert);
+        }
     }
 }
