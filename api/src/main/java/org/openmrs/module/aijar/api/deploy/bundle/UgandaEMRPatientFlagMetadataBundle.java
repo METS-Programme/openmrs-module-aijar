d54b25c9fc8ded80c9dcf338e714edbeb488bc57
Stephen S. Musoke <ssmusoke@gmail.com>
2018-01-23 19:57:55 +0300

    Add the ability to show patient status information on the dashboard based on information entered  (#317)
    * Add ability to deploy patient flags via metadata deploy
    
    * Changed patient flags to use the api module instead of the omod
    
    * Changed role reference from a uuid to the name of the role
    
    * Removed the tag evaluator since the default is an SQL Flag Evaluator
    
    * Added patient flags as a required module
    
    * Added the info message for module startup only when there are no exceptions
    
    * Updated the module versions to the latest releases from Reference App 2.7 release train
    
    * Added the first round of patient flags
    
    * Updated to install available flags
    
    * Enable the recent visits dashboard widget
    
    * Update of DUE_FOR_FIRST_VIRAL_LOAD to enable additional queries for ANC
    
    * Updated uicommons and core versions to fix issue with breaking dashboard widgets
    
    * Added Exposed Infant and Viral Load patient flags

---
+++ api/src/main/java/org/openmrs/module/aijar/api/deploy/bundle/UgandaEMRPatientFlagMetadataBundle.java
@@ -0,0 +1,48 @@
+package org.openmrs.module.aijar.api.deploy.bundle;
+
+import org.openmrs.module.aijar.metadata.core.Flags;
+import org.openmrs.module.aijar.metadata.core.Priorites;
+import org.openmrs.module.aijar.metadata.core.Tags;
+import org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle;
+import org.openmrs.module.patientflags.metadatadeploy.bundle.PatientFlagMetadataBundle;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Installs metadata for Patient Flags
+ */
+@Component
+public class UgandaEMRPatientFlagMetadataBundle extends PatientFlagMetadataBundle {
+
+    /**
+     * @see AbstractMetadataBundle#install()
+     */
+    public void install() throws Exception {
+        // Tags
+        log.info("Installing Patient flag tags");
+        install(Tags.PATIENT_STATUS);
+
+        // Priorites
+        log.info("Installing patient flag priorities");
+        install(Priorites.GREEN);
+        install(Priorites.RED);
+        install(Priorites.ORANGE);
+
+        // Flags
+        log.info("Installing flags");
+        install(Flags.DUE_FOR_FIRST_VIRAL_LOAD);
+        install(Flags.OVERDUE_FOR_FIRST_VIRAL_LOAD);
+        install(Flags.MISSED_APPOINTMENT);
+        install(Flags.UPCOMING_APPOINTMENT);
+        install(Flags.PATIENT_LOST);
+        install(Flags.PATIENT_LOST_TO_FOLLOWUP);
+        install(Flags.DUE_FOR_ROUTINE_VIRAL_LOAD);
+        install(Flags.OVERDUE_FOR_ROUTINE_VIRAL_LOAD);
+        install(Flags.DUE_FOR_FIRST_DNA_PCR);
+        install(Flags.OVERDUE_FOR_FIRST_DNA_PCR);
+        install(Flags.DUE_FOR_SECOND_DNA_PCR);
+        install(Flags.OVERDUE_FOR_SECOND_DNA_PCR);
+        install(Flags.DUE_FOR_RAPID_TEST);
+        install(Flags.OVERDUE_FOR_RAPID_TEST);
+    }
+}
