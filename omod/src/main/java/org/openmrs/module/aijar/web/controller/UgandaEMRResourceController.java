e7caf5da278cd9a1be092834ded6d780153891b1
reagan <reaganment@gmail.com>
2020-06-09 09:22:20 +0300

    Aijar-ISS-9: Add rest end point to check for public holiday

---
+++ omod/src/main/java/org/openmrs/module/aijar/web/controller/UgandaEMRResourceController.java
@@ -0,0 +1,16 @@
+package org.openmrs.module.aijar.web.controller;
+
+import org.openmrs.module.aijar.AijarConstants;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.MainResourceController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rest/" + RestConstants.VERSION_1 + "/" + AijarConstants.UGANDAEMR_MODULE_ID)
+public class UgandaEMRResourceController extends MainResourceController{
+    @Override
+    public String getNamespace() {
+        return RestConstants.VERSION_1 + "/" + AijarConstants.UGANDAEMR_MODULE_ID;
+    }
+} 
