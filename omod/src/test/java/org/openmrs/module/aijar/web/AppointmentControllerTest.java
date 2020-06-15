5242509aaa63e33d9b3c4cd18a51e1920b8689fc
reagan-meant <47450907+reagan-meant@users.noreply.github.com>
2020-06-15 11:45:17 +0300

    ISS-5: Add a rest endpoint to fetch number of patients booked for a given return date (#487)

---
+++ omod/src/test/java/org/openmrs/module/aijar/web/AppointmentControllerTest.java
@@ -0,0 +1,78 @@
+package org.openmrs.module.aijar.web;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.RestControllerTestUtils;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class AppointmentControllerTest extends RestControllerTestUtils {
+
+    @Before
+    public void setUp() throws Exception {
+        executeDataSet("org/openmrs/module/aijar/include/standardTestDataset.xml");
+        executeDataSet("org/openmrs/module/aijar/include/appointmentDataSet.xml");
+    }
+
+    @Test
+    public void shouldReturnPatientsWhenNoSpecifiedEncounterUuids() throws Exception {
+        String requestURI = "/rest/" + RestConstants.VERSION_1 + "/ugandaemr/appointmentcount";
+        MockHttpServletRequest request = new MockHttpServletRequest(RequestMethod.GET.toString(), requestURI);
+        request.addParameter("nextAppointmentDate", "14/08/2008");
+        SimpleObject result = deserialize(handle(request));
+        Assert.assertEquals("2008-08-14",PropertyUtils.getProperty(result, "appointmentdate"));
+        Assert.assertEquals("",PropertyUtils.getProperty(result, "encountertypeUuid"));
+        Assert.assertEquals(3,PropertyUtils.getProperty(result, "appointmentcount"));
+
+    }
+
+    @Test
+    public void shouldReturnPatientsWithSpecifiedEncounterUuid() throws Exception {
+        String requestURI = "/rest/" + RestConstants.VERSION_1 + "/ugandaemr/appointmentcount";
+        MockHttpServletRequest request = new MockHttpServletRequest(RequestMethod.GET.toString(), requestURI);
+        request.addParameter("nextAppointmentDate", "14/08/2008");
+        request.addParameter("encounterTypeUuid", "8d5b2be0-c2cc-11de-8d13-0010c6dffd0f");
+        SimpleObject result = deserialize(handle(request));
+        Assert.assertEquals("2008-08-14",PropertyUtils.getProperty(result, "appointmentdate"));
+        Assert.assertEquals("8d5b2be0-c2cc-11de-8d13-0010c6dffd0f",PropertyUtils.getProperty(result, "encountertypeUuid"));
+        Assert.assertEquals(2,PropertyUtils.getProperty(result, "appointmentcount"));
+    }
+
+    @Test
+    public void shouldReturnPatientsWithSpecifiedEncounterUuids() throws Exception {
+        String requestURI = "/rest/" + RestConstants.VERSION_1 + "/ugandaemr/appointmentcount";
+        MockHttpServletRequest request = new MockHttpServletRequest(RequestMethod.GET.toString(), requestURI);
+        request.addParameter("nextAppointmentDate", "14/08/2008");
+        request.addParameter("encounterTypeUuid",
+                "8d5b2be0-c2cc-11de-8d13-0010c6dffd0f,334bf97e-28e2-4a27-8727-a5ce31c7cd66");
+        SimpleObject result = deserialize(handle(request));
+        Assert.assertEquals("2008-08-14",PropertyUtils.getProperty(result, "appointmentdate"));
+        Assert.assertEquals("8d5b2be0-c2cc-11de-8d13-0010c6dffd0f,334bf97e-28e2-4a27-8727-a5ce31c7cd66",PropertyUtils.getProperty(result, "encountertypeUuid"));
+        Assert.assertEquals(3,PropertyUtils.getProperty(result, "appointmentcount"));
+    }
+
+    @Test
+    public void shouldReturnNoPatientsWhenSpecifiedDateDoesntHaveEncounter() throws Exception {
+        String requestURI = "/rest/" + RestConstants.VERSION_1 + "/ugandaemr/appointmentcount";
+        MockHttpServletRequest request = new MockHttpServletRequest(RequestMethod.GET.toString(), requestURI);
+        request.addParameter("nextAppointmentDate", "01/01/1980");
+        SimpleObject result = deserialize(handle(request));
+        Assert.assertEquals(0,PropertyUtils.getProperty(result, "appointmentcount"));
+    }
+
+    @Test
+    public void shouldReturnNoPatientsWhenSpecifiedEncounterUuidDoesntExist() throws Exception {
+        String requestURI = "/rest/" + RestConstants.VERSION_1 + "/ugandaemr/appointmentcount";
+        MockHttpServletRequest request = new MockHttpServletRequest(RequestMethod.GET.toString(), requestURI);
+        request.addParameter("nextAppointmentDate", "14/08/2008");
+        request.addParameter("encounterTypeUuid", "8p5b2be0-c2cc-11de-8d13-0010c6dffd0f");
+        SimpleObject result = deserialize(handle(request));
+        Assert.assertEquals(0,PropertyUtils.getProperty(result, "appointmentcount"));
+    }
+}
+
